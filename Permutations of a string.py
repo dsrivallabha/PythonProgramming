568e1d091cac1df2d6b4d253ec321855b5290d8a
Sri Vallabha Deevi <srivallabhad@yahoo.com>
2017-12-11 10:08:39 +0530

    Update Permutations of a string.py
    Updated to handle repetitions in characters

--- Permutations of a string.py
+++ Permutations of a string.py
@@ -1,4 +1,5 @@
 ## Program to return all the permutations of a string
+
 import numpy, math
 
 #### Function
@@ -22,6 +23,9 @@ def returnperm(string):
     #counter to fill the first element
     counter1 = 0
     
+    #used character list: to avoid repetitions
+    usedchar = []
+    
     #loop over length of string, to pick the first element
     for kk in range(slen):
         # newstring is the shuffled string, initialize it to an empty list
@@ -29,42 +33,49 @@ def returnperm(string):
         
         #first element is fc
         fc = slist[counter1]
-        #this is appended to the newstring
-        newstring.append(fc)
+        #print fc, usedchar
         
-        #remstring is list of all other characters, except fc        
-        remstring = [x for x in slist if x != fc]
-        
-        # if len of remstring =1, this is the last character, hence a shuffled string can be formed
-        if (len(remstring) == 1):
-            shufstring = ''.join(newstring+remstring)
-            #add this shuffled string to return list
-            retlist.append(shufstring)
+        if ((kk == 0) or (fc not in usedchar)):
             
-        else:
-        # len of remstring is not 1, call the function recursively, to return a list of permutations
-            newlist = returnperm(remstring)
-            #create shuffled strings from each of the permutations of this partial string
-            for ii in newlist:
-                shufstring = fc+ ii
+           #this is appended to used char as well
+            usedchar.append(fc)
+            
+            #this is appended to the newstring
+            newstring.append(fc)
+            
+            #remstring is list of all other characters, except fc        
+            if (kk == 0):
+                remstring = slist[1:]
+            else:
+                remstring = [x for x in slist if x != fc]
+ 
+            # if len of remstring =1, this is the last character, hence a shuffled string can be formed
+            if (len(remstring) == 1):
+                shufstring = ''.join(newstring+remstring)
+                #add this shuffled string to return list
                 retlist.append(shufstring)
-                     
-        #update counter to fill the next element as the first element in the string
+
+            else:
+            # len of remstring is not 1, call the function recursively, to return a list of permutations
+                newlist = returnperm(remstring)
+                #create shuffled strings from each of the permutations of this partial string
+                for ii in newlist:
+                    shufstring = fc+ ii
+                    retlist.append(shufstring)
+
+        #update counter to fill choose the next element as the first element in the string
         counter1 += 1
     
     return (retlist)   
 
 
 #### Input
-string = raw_input("Please enter the string:  ")
-print "you entered:  ", string
+string = 'aav'
 
 print 'length of string is', len(string)
-print 'number of permutations possible is', math.factorial(len(string))
-
 
 permutations = returnperm(string)
 
-print 'number of permutations returned are', len(permutations)
+print 'number of permutations are', len(permutations)
 
 print permutations
